989bbe83997cd573cf85d7a21bfda6708d480e3f
快乐浪子哥 <hulangfy@163.com>
2024-07-26 09:24:56 +0800

    Update

--- src/facade/Jwt.php
+++ src/facade/Jwt.php
@@ -7,15 +7,15 @@ namespace think\facade;
 use think\Facade;
 
 /**
- * ThinkPHP Jwt Facade
+ * ThinkPHP 8.0+ Jwt 验证
  * @see \think\facade\Jwt
  * @package think\facade\Jwt
  * @mixin \think\facade\Jwt
- * @method static mixed|string|Exception getToken($data = []) 获取token
- * @method static mixed|array|Exception Check($token = '') 验证令牌(Token)的有效性
+ * @method static mixed|string getToken($data = []) 获取token
+ * @method static mixed|array Check($token = '') 验证令牌(Token)的有效性
  * @method static mixed|string Refresh($token = '') 刷新令牌
- * @method static mixed|bool Logout($token) 注销令牌
- * @method static mixed|array|Exception Parse($token) 解析并验证JWT令牌
+ * @method static mixed|bool Logout($token = '') 注销令牌
+ * @method static mixed|array|Exception Parse($token = '', $type = 0) 解析并验证JWT令牌
  */
 class Jwt extends Facade
 {
