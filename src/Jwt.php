2d1110841f7834ab886be1ccf13a7afb9274e7a9
快乐浪子哥 <4961831+hulang@users.noreply.github.com>
2024-09-18 09:44:04 +0800

    upddate

--- src/Jwt.php
+++ src/Jwt.php
@@ -19,13 +19,13 @@ class Jwt
     protected $secret = '';
     protected $exp = 3600; //一小时
     protected $refresh = 604800; //七天
-    const ENCRYPT_EORROR = 50001; //jwt加密算法运算时异常
-    const JWT_SECRET_MISS = 50002; //jwt加密秘钥值未设置
-    const INVALID_TOKEN = 40001; //token格式不正确不合法,异常的token
-    const TOKEN_EXPIRE = 20001; //token过期,需要刷新
-    const TOKEN_EXPIRE_LONG = 20002; //token过期,过期时间超过上限
-    const TOKEN_LOGOUT = 20003; //token已经被注销
-    const TOKEN_DOMAIN = 20004; //token已经被注销
+    public const ENCRYPT_EORROR = 50001; //jwt加密算法运算时异常
+    public const JWT_SECRET_MISS = 50002; //jwt加密秘钥值未设置
+    public const INVALID_TOKEN = 40001; //token格式不正确不合法,异常的token
+    public const TOKEN_EXPIRE = 20001; //token过期,需要刷新
+    public const TOKEN_EXPIRE_LONG = 20002; //token过期,过期时间超过上限
+    public const TOKEN_LOGOUT = 20003; //token已经被注销
+    public const TOKEN_DOMAIN = 20004; //token已经被注销
 
     /**
      * 根据配置文件设置相关参数
